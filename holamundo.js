5f17844f6d37162915502c36ce378b8539924548
Juanca752 <juancgb00@gmail.com>
2023-04-20 09:20:18 -0600

    Primer commit de Http

---
+++ holamundo.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+//Establecemos la ip de winodws y de Ubuntu
+const hostname = '127.0.0.1';
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+    res.statusCode = 200;
+
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.end('Hola Mundo\n');
+  });
+
+  server.listen(port, hostname, () => {
+    console.log(`El servidor se esta ejecutando en http://${hostname}:${port}/`);
+  });
+  
